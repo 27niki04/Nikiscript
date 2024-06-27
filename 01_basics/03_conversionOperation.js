bceb9321e82ff8060ff9aaaa41207e8fe7519516
27niki04 <157293867+27niki04@users.noreply.github.com>
2024-06-27 02:40:49 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score="ABC";
+
+//console.log(typeof score);
+
+let ValueInNumber=Number(score);
+//console.log(typeof ValueInNumber);
+//console.log(ValueInNumber);
+
+//"33" => 33
+//"33abc" => 33abc
+//true=>1 // false => 0
+
+let isLoggedIn="";
+
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+
+//console.log(booleanIsLoggedIn);
+
+//1 => true; 0=> false
+// "" =>false
+//"Nikita" =>true
+
+let someNumber=33
+
+let stringNumber= String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
