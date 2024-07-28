366401ba0ee835b72a85e82b1fd843bda597235d
27niki04 <157293867+27niki04@users.noreply.github.com>
2024-07-28 14:23:59 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,10 @@
+// primitive Type:
+
+// 7types: string,number,boolean,null,undefined,symbol,BigInt
+
+//js is a dynamically types language-variables can hold values of different types during runtime
+
+//Reference (Non-primitive)Type:
+
+//Arrays,Objects,Functions 
+
