62c0ba2ce86ced47fd8f3cf1cb6ca1ffb77e1495
27niki04 <157293867+27niki04@users.noreply.github.com>
2024-07-29 00:44:44 +0000

    datatypes-summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -2,9 +2,27 @@
 
 // 7types: string,number,boolean,null,undefined,symbol,BigInt
 
-//js is a dynamically types language-variables can hold values of different types during runtime
+//datatype of null=>object
 
-//Reference (Non-primitive)Type:
+//js is a dynamically types language-variables can hold values of different types during runtime
+  const id=Symbol('123')
+  const anotherId=Symbol('123')
+
+  console.log(id===anotherId); //=>false
+ 
+  const bigNumber=12345678945613n
+
+//Reference (Non-primitive)Type:  =>datatype:object
 
 //Arrays,Objects,Functions 
 
+const heros=["A","B","C"];
+
+let myObj={
+  name:"Nikita",
+  age:20,
+}
+
+ const myFunction=function(){
+  console.log("Hello World");
+ }
