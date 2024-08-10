a910fd94de1ac5dacbfa5bc4b5635f9da8f25b62
27niki04 <157293867+27niki04@users.noreply.github.com>
2024-08-10 14:19:15 +0000

    Numbers and Maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,65 @@
+//+++++++++++++++++++++++++++++++++NUMBERS++++++++++++++++++++++++++++++++++++++++++++++
+
+
+const score=100
+//console.log(score); =>100
+
+const balance= new Number(100)
+//console.log(balance); =>[Number:100]
+
+//console.log(balance.toString()); =>100
+
+//console.log(balance.toString().length); =>3
+
+//console.log(balance.toFixed(2)); =>100.00
+
+const num2=123.8969
+//console.log(num2.toPrecision(3)); =>124
+
+const num3=1123.896
+//console.log(num3.toPrecision(3)); =>1.12e+3
+
+const hundreds=1000000
+//console.log(hundreds.toLocaleString()); =>1,000,000
+//console.log(hundreds.toLocaleString("en-IN")); =>10,00,000
+
+
+//++++++++++++++++++++++++++++++++++++++MATHS++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//console.log(Math); =>Object [Math] {}
+
+//console.log(Math.abs(-4));  =>absolute value:4
+
+//console.log(Math.round(4.5));  =>5
+
+//console.log((Math.ceil(4.2)));  =>5
+
+//console.log(Math.floor(4.9));  =>4
+
+//console.log(Math.min(4,6,9,8));  =>4
+
+//console.log(Math.max(4,6,9,8));  =>9
+
+//console.log(Math.random());
+//console.log(Math.random()*10 + 1);
+
+
+const max=20
+const min=10
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
