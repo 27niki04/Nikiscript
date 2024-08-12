c4e44327bb5387c83a9dfcfff42b75e14f78120f
27niki04 <157293867+27niki04@users.noreply.github.com>
2024-08-12 14:22:14 +0000

    Date and time

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,23 @@
+//Dates
+
+let myDate= new Date()
+//console.log(myDate.toDateString());  =>Mon Aug 12 2024
+ 
+//console.log(myDate.toString());  =>Mon Aug 12 2024 14:05:30 GMT+0000 (Coordinated Universal Time)
+
+//console.log(myDate.toLocaleDateString());  =>8/12/2024
+
+//console.log(typeof myDate);  =>object
+
+let CreatedDate=new Date(2024,0,24)
+//console.log(CreatedDate.toDateString());  =>Wed Jan 24 2024
+
+
+let CreatedDate2=new Date(2024,0,24,5,3)
+//console.log(CreatedDate2.toLocaleString());  =>1/24/2024, 5:03:00 AM
+
+
+
+
+
+
