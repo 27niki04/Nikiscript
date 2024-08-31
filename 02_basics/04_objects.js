9628751d4004b64dbd22bc6fb09049bcd09b192e
27niki04 <157293867+27niki04@users.noreply.github.com>
2024-08-31 18:04:09 +0000

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,2 +1,27 @@
-const User=new Object()
-console.log(User);
+//const User=new Object()
+const User={}
+User.id="123"
+User.name="Nikita"
+User.isLoggedIn=false
+// 
+
+const regularUser={
+    email:"nikita123@gmail.com",
+    fullname:{
+        userfullname:{
+        firstname:"Nikita",
+        lastname:"Rani"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname);
+
+ const obj1={1:"a",2:"b"}
+const obj2={3:"c",4:"d"}
+
+//const obj3={obj1,obj2} =>{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'c', '4': 'd' } }
+
+
+//const obj3=Object.assign(obj1,obj2) =>{ '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
+console.log(obj3);
