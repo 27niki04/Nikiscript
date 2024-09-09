059e044d9a6c00b30269858a8566f9fad8a30c2d
27niki04 <157293867+27niki04@users.noreply.github.com>
2024-09-09 14:54:57 +0000

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -19,9 +19,42 @@ const regularUser={
 
  const obj1={1:"a",2:"b"}
 const obj2={3:"c",4:"d"}
+const obj4={5:"e",6:"f"}
 
 //const obj3={obj1,obj2} =>{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'c', '4': 'd' } }
 
 
 //const obj3=Object.assign(obj1,obj2) =>{ '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
-console.log(obj3);
+//const obj3=Object.assign({},obj1,obj2,obj4)=>{ '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e', '6': 'f' }
+
+//const obj3={...obj1,...obj2,...obj4}  =>spread operator
+//console.log(obj3); 
+const users=[
+    {id:1,
+    email:"h@123.gmail"
+    },
+    {
+        id:2,
+        email:"p@256.gmail.com"
+    },
+    {
+        id:3,
+        email:"123@J"
+    }
+]
+
+users[1].email
+// console.log(User); =>{ id: '123', name: 'Nikita', isLoggedIn: false }
+
+
+// console.log(Object.keys(User)); =>[ 'id', 'name', 'isLoggedIn' ]
+//console.log(Object.values(User)); =>[ '123', 'Nikita', false ]
+//console.log(Object.entries(User));  =>[ [ 'id', '123' ], [ 'name', 'Nikita' ], [ 'isLoggedIn', false ] ]
+
+//console.log(User.hasOwnProperty('isLoggedin'));   =>false
+
+
+
+
+
+
