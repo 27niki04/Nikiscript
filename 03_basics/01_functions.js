7c7b449dd20fdc18458dcb4f84e2f529275e3885
27niki04 <157293867+27niki04@users.noreply.github.com>
2024-09-12 15:56:52 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,20 @@
+// function addTwoNumbers(number1, number2){
+//     console.log(number1+number2);
+    
+// }
+
+// const result= addTwoNumbers(3,5)  =>8
+
+// console.log("Result: ",result);  =>Result: undefined
+
+
+ /*****************/
+
+ 
+// function addition(num1 ,num2){
+//     let res=num1 +num2
+//     return res
+// }
+
+// const res=addition(3,5)
+// console.log("Result: ",res);  =>Result:  8
