7793abd2455808b75318709ca9cc0ac0e03c9358
27niki04 <157293867+27niki04@users.noreply.github.com>
2024-09-27 06:35:37 +0000

    scopes

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -28,7 +28,7 @@ function loginUserMessage(username){
     }
     return `${username} just logged in`
 }
-console.log(loginUserMessage());
+//console.log(loginUserMessage());
 
 
 //console.log(loginUserMessage("nikita"));  =>nikita just logged in
@@ -36,4 +36,38 @@ console.log(loginUserMessage());
 //console.log(loginUserMessage(""));  => just logged in
 
 // console.log(loginUserMessage());  =>undefined just logged in
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200,500,600));  =>[ 200, 500, 600 ]
+
+function calculateCartPrice(val1,val2,...num1)
+{
+    return num1
+}
+
+// console.log(calculateCartPrice(200,500,600,2000));  =>[ 600, 2000 ]
+
+// const user={
+//     username:"nikita",
+//     price:199
+// }
+
+// function handleObject(anyobject){
+//     console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+// }
+
+// handleObject(user)  =>Username is nikita and price is 199
+
+const arr=[200,400,600]
+
+function returnSecondValue(getarray){
+    return getarray[1]
+}
+
+// console.log(returnSecondValue(arr)); =>400
+// console.log(returnSecondValue([200,400,600]));  =>400
 
