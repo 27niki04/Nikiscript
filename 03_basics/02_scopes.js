b8b3dfc9707f0d5fd8537feda34f8e20dd8905ae
27niki04 <157293867+27niki04@users.noreply.github.com>
2024-09-27 07:40:22 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,9 +1,49 @@
-let a=10
-const b=20
-var c=30
+// let a=10
+// const b=20
+// var c=30
 
-console.log(a);
-console.log(b);
-console.log(c);
+// // console.log(a);
+// // console.log(b);
+// // console.log(c);
 
+// function one(){
+//     const username="nikita"
+
+//     function two(){
+//         const website="youtube"
+//         console.log(username);
+        
+//     }
+
+//     // console.log(website);
+
+//     two()
+    
+// }
+
+// one()
+
+// if(true){
+//     const username="nikita"
+//     if(username==="nikita"){
+//         const website="youtube"
+//         console.log(username+website);
+        
+//     }
+//     console.log(website);
+    
+
+// }
+// console.log(username);
+
+function addOne(num){
+    return num+1
+}
+//console.log(addOne(5)); =>6
+
+const addTwo= function(num){
+    return num+2
+}
+
+// console.log(addTwo(5)); =>7
 
